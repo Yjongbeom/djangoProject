ab13ab4f418d5b425b412fdbfdc9b30ba16106bf
Yjongbeom <yukjb5154@naver.com>
2023-12-29 18:39:47 +0900

    Project

--- djangoProject/models.py
+++ djangoProject/models.py
@@ -51,36 +51,3 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
-
-class Login(models.Model):
-    username = models.IntegerField(unique=True)
-    password = models.CharField(max_length=255)
-
-class StudentSession(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE, db_column="username")
-    session = models.CharField(max_length=255, unique=True)
-
-class Week(models.Model):
-    user_id = models.ForeignKey(User, related_name="weeks", on_delete=models.CASCADE, db_column="user_id")
-    week_id = models.AutoField(primary_key=True)
-    weeks = models.IntegerField(help_text="Weeks")
-
-def upload_to_assignments(instance, filename):
-    return f"assignments/{instance.week_id.user_id.student_id}/{instance.week_id.weeks}/{filename}"
-
-class Assignment(models.Model):
-    week_id = models.ForeignKey(Week, related_name="assignments", on_delete=models.CASCADE, db_column="week_id")
-    weeks = models.IntegerField(help_text="Weeks")
-    assignment_id = models.AutoField(primary_key=True)
-    assignment_title = models.CharField(help_text="assignment_title", max_length=100)
-    submission_status = models.CharField(max_length=1, choices=[('T', 'Submitted'), ('F', 'Not Submitted'), ('L', 'Late')])
-    file = models.FileField(upload_to=upload_to_assignments)
-    submission_time = models.DateTimeField()
-
-class Notice(models.Model):
-    user_id = models.ForeignKey(User, related_name="notices", on_delete=models.CASCADE, db_column="user_id")
-    notice_id = models.AutoField(primary_key=True)
-    notice_title = models.CharField(help_text="notice_title", max_length=100)
-    notice_comment = models.TextField(help_text="notice_comment")
-    notice_time = models.DateTimeField(default=timezone.now)
-    file = models.FileField(upload_to='notice/')
