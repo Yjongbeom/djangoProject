d04ad2724fcdb6ef0e081fb40b4c36ec5e228d19
Yjongbeom <yukjb5154@naver.com>
2023-12-30 03:18:11 +0900

    Project

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -49,6 +49,11 @@ class AuthAPIView(APIView):
                 print(3)
                 print(data)
                 serializer = TokenRefreshSerializer(data=data)
+                try:
+                    serializer.is_valid(raise_exception=True)
+                except Exception as e:
+                    print(f'Error: {e}')
+                    # Further handling
                 if serializer.is_valid(raise_exception=True):
                     access = serializer.data.get('access', None)
                     print(4)
