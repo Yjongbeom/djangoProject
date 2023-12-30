4eb45ffd8f55e4118f99f19a428b926113ddf3da
Yjongbeom <yukjb5154@naver.com>
2023-12-30 17:46:06 +0900

    Project

--- djangoProject/serializers.py
+++ djangoProject/serializers.py
@@ -4,6 +4,7 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    access = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = (
